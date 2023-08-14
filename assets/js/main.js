bc9cacaa6edb5f9d28f2bf15512308e6c2fdc712
Mthokozisi Hector Hlatshwayo <79380754+M-h-sh@users.noreply.github.com>
2023-08-14 14:10:47 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -238,29 +238,7 @@
       }
     }
   });
-// JavaScript to toggle dark mode
-const darkModeToggle = document.getElementById("dark-mode-toggle");
-const body = document.body;
 
-darkModeToggle.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-});
-// Check if dark mode preference is stored in local storage
-const isDarkMode = localStorage.getItem("darkMode");
-
-if (isDarkMode) {
-  body.classList.add("dark-mode");
-}
-
-// Toggle dark mode and update local storage
-darkModeToggle.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-  if (body.classList.contains("dark-mode")) {
-    localStorage.setItem("darkMode", "enabled");
-  } else {
-    localStorage.removeItem("darkMode");
-  }
-});
 
   /**
    * Animation on scroll
