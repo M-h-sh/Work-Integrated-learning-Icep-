d82a6a9c5580d80e7aec248d534b56f44b32c9b9
Mthokozisi Hector Hlatshwayo <79380754+M-h-sh@users.noreply.github.com>
2023-08-14 14:45:30 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,27 +19,7 @@
       return document.querySelector(el)
     }
   }
-document.addEventListener("DOMContentLoaded", function () {
-  const darkModeSwitch = document.getElementById("darkModeSwitch");
-  const body = document.body;
 
-  // Check dark mode preference on page load
-  const isDarkMode = localStorage.getItem("darkMode");
-  if (isDarkMode === "enabled") {
-    body.classList.add("dark");
-    darkModeSwitch.checked = true;
-  }
-
-  darkModeSwitch.addEventListener("change", function () {
-    if (this.checked) {
-      body.classList.add("dark");
-      localStorage.setItem("darkMode", "enabled");
-    } else {
-      body.classList.remove("dark");
-      localStorage.removeItem("darkMode");
-    }
-  });
-});
   /**
    * Easy event listener function
    */
