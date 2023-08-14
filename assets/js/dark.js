4a65bbf87df27d8107731f90514792044f982025
Mthokozisi Hector Hlatshwayo <79380754+M-h-sh@users.noreply.github.com>
2023-08-14 14:30:51 +0200

    Update dark.js

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,18 +1,21 @@
-const darkModeToggle = select("#dark-mode-toggle");
+document.addEventListener("DOMContentLoaded", function () {
+  const darkModeSwitch = document.getElementById("darkModeSwitch");
   const body = document.body;
 
-  darkModeToggle.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-    // Store the user's preference in local storage
-    if (body.classList.contains("dark-mode")) {
+  // Check dark mode preference on page load
+  const isDarkMode = localStorage.getItem("darkMode");
+  if (isDarkMode === "enabled") {
+    body.classList.add("dark");
+    darkModeSwitch.checked = true;
+  }
+
+  darkModeSwitch.addEventListener("change", function () {
+    if (this.checked) {
+      body.classList.add("dark");
       localStorage.setItem("darkMode", "enabled");
     } else {
+      body.classList.remove("dark");
       localStorage.removeItem("darkMode");
     }
   });
-
-  // Check for dark mode preference on page load
-  const isDarkMode = localStorage.getItem("darkMode");
-  if (isDarkMode === "enabled") {
-    body.classList.add("dark-mode");
-  }
+});
