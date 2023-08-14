3dbbae39670bd0dac2207da157f2ea44326e494a
Mthokozisi Hector Hlatshwayo <79380754+M-h-sh@users.noreply.github.com>
2023-08-14 14:19:24 +0200

    Update dark.js

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,23 +1,18 @@
-// JavaScript to toggle dark mode
-const darkModeToggle = document.getElementById("dark-mode-toggle");
-const body = document.body;
+const darkModeToggle = select("#dark-mode-toggle");
+  const body = document.body;
 
-darkModeToggle.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-});
-// Check if dark mode preference is stored in local storage
-const isDarkMode = localStorage.getItem("darkMode");
+  darkModeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    // Store the user's preference in local storage
+    if (body.classList.contains("dark-mode")) {
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      localStorage.removeItem("darkMode");
+    }
+  });
 
-if (isDarkMode) {
-  body.classList.add("dark-mode");
-}
-
-// Toggle dark mode and update local storage
-darkModeToggle.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-  if (body.classList.contains("dark-mode")) {
-    localStorage.setItem("darkMode", "enabled");
-  } else {
-    localStorage.removeItem("darkMode");
+  // Check for dark mode preference on page load
+  const isDarkMode = localStorage.getItem("darkMode");
+  if (isDarkMode === "enabled") {
+    body.classList.add("dark-mode");
   }
-});
